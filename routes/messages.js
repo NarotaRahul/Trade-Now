ba7690ee463eac8cc51e7902313a05a93333d701
Rahul <narotaammy@gmail.com>
2025-11-25 20:10:00 +0530

    git commite-m "add all"

--- routes/messages.js
+++ routes/messages.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const Message = require('../models/Message');
 
-router.post('/',
-  [
-    body('sender').notEmpty().withMessage('Sender is required'),
-    body('recipient').notEmpty().withMessage('Recipient is required'),
-    body('content').notEmpty().withMessage('Content is required')
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      const msg = new Message(req.body);
-      await msg.save();
-      res.status(201).json(msg);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+router.post('/', async (req, res) => {
+  try {
+    const msg = new Message(req.body);
+    await msg.save();
+    res.status(201).json(msg);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-);
+});
 
 router.get('/', async (req, res) => {
   const messages = await Message.find();
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const User = require('../models/User');
 
-router.post('/login', 
-  [
-    body('username').notEmpty().withMessage('Username is required'),
-    body('email').isEmail().withMessage('Valid email is required'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      const user = new User(req.body);
-      await user.save();
-      res.status(201).json(user);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+router.post('/', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
-);
+});
 
 router.get('/', async (req, res) => {
   const users = await User.find();
